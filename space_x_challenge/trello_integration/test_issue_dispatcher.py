192714bc8640953b8e819a2bf094785db0d30456
Edwin Abot <edwinabot@gmail.com>
2022-07-19 23:17:48 -0300

    TDD the bug requests

---
+++ space_x_challenge/trello_integration/test_issue_dispatcher.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+
+from trello_integration.models import Bug
+
+class BugsTestCase(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.path = '/api/issues/'
+
+    def test_valid_bug_request(self):
+        """
+        Testing with a valid bug request.
+        """
+        description = "Houston... we have a problem"
+        payload = {
+            "type": "bug",
+            "description": description
+        }
+        response = self.client.post(self.path, payload, format='json')
+        self.assertEqual(200, response.status_code)
+        self.assertTrue(Bug.objects.filter(description=description).exists())
+
+    def test_bug_request_missing_description(self):
+        """
+        Testing with a missing attribute, in this case, the description
+        """
+        payload = {
+            "type": "bug",
+        }
+        response = self.client.post(self.path, payload, format='json')
+        self.assertEqual(400, response.status_code)
--- space_x_challenge/trello_integration/views.py
+++ space_x_challenge/trello_integration/views.py
@@ -7,4 +7,8 @@ from rest_framework.response import Response
 
 class IssueDispatcherView(APIView):
     def post(self, request: Request, *args, **kwargs) -> Response:
+        """
+        This view processes, stores and dispatches the right kind of issue to the
+        trello worker
+        """
         return Response("Not Implemented", status=status.HTTP_501_NOT_IMPLEMENTED)
