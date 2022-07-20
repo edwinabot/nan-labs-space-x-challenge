ff995faa423b3925940ac6421d0d370b716454fb
Edwin Abot <edwinabot@gmail.com>
2022-07-20 00:14:39 -0300

    Handling Issue type

--- space_x_challenge/trello_integration/test_issue_dispatcher.py
+++ space_x_challenge/trello_integration/test_issue_dispatcher.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from trello_integration.models import Bug
+from trello_integration.models import Bug, Issue
 
 
 class BugsTestCase(TestCase):
@@ -18,7 +18,10 @@ class BugsTestCase(TestCase):
         payload = {"type": "bug", "description": description}
         response = self.client.post(self.path, payload, format="json")
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
-        self.assertTrue(Bug.objects.filter(description=description).exists())
+        self.assertEqual(description, response.data["description"])
+        self.assertTrue(
+            Bug.objects.filter(description=response.data["description"]).exists()
+        )
 
     def test_bug_request_missing_description(self):
         """
@@ -26,6 +29,37 @@ class BugsTestCase(TestCase):
         """
         payload = {
             "type": "bug",
+        }
+        response = self.client.post(self.path, payload, format="json")
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+
+
+class IssuesTestCase(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.path = "/api/issues/"
+        self.issue_type = "issue"
+
+    def test_valid_issue_request(self):
+        """
+        Testing with a valid Issue request.
+        """
+        description = (
+            "There's so much I should write here, but I don't have the time for that"
+        )
+        title = "The best Issue title you've ever seen"
+        payload = {"type": self.issue_type, "title": title, "description": description}
+        response = self.client.post(self.path, payload, format="json")
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertTrue(Issue.objects.filter(title=response.data['title']).exists())
+
+    def test_issue_request_missing_title(self):
+        """
+        Testing with a missing attribute, in this case, the description
+        """
+        payload = {
+            "type": self.issue_type,
+            "description": "Some description over here"
         }
         response = self.client.post(self.path, payload, format="json")
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
--- space_x_challenge/trello_integration/views.py
+++ space_x_challenge/trello_integration/views.py
@@ -4,8 +4,12 @@ from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from trello_integration.models import Bug
-from trello_integration.serializers import BugSerializer
+from trello_integration.serializers import (
+    BugSerializer,
+    IssueSerializer,
+    TaskSerializer,
+)
+
 
 class IssueDispatcherView(APIView):
     type_bug = "bug"
@@ -18,33 +22,27 @@ class IssueDispatcherView(APIView):
         trello worker
         """
         requested_type = request.data.pop("type")
-        issue_handler = self.get_issue_handler(requested_type)
-        result = issue_handler(request.data)
+        serializer = self.get_issue_serializer(requested_type)
+        result = self.handle_payload(request.data, serializer)
         return result
 
-    def get_issue_handler(self, issue_type):
+    def get_issue_serializer(self, issue_type):
         """
-        Returns the right handler for an issue type
+        Returns the right Serializer for an issue type
         """
         return {
-            self.type_bug: self.handle_bug,
-            self.type_task: self.handle_task,
-            self.type_issue: self.handle_issue,
+            self.type_bug: BugSerializer,
+            self.type_task: TaskSerializer,
+            self.type_issue: IssueSerializer,
         }[issue_type]
 
-    def handle_bug(self, payload):
+    def handle_payload(self, payload, serializer):
         """
         This method processes Bug requests
         """
-        bug_serializer = BugSerializer(data=payload)
+        bug_serializer = serializer(data=payload)
         if bug_serializer.is_valid():
             bug_serializer.save()
             return Response(bug_serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(bug_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def handle_issue(self, payload):
-        return Response("Not Implemented", status=status.HTTP_501_NOT_IMPLEMENTED)
-
-    def handle_task(self, payload):
-        return Response("Not Implemented", status=status.HTTP_501_NOT_IMPLEMENTED)
